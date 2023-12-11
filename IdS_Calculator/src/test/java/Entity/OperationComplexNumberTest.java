013a7f4aaf671758053e49ed084b878d866e2d41
filsab <96667199+filsab@users.noreply.github.com>
2023-12-11 19:08:27 +0100

    Update OperationComplexNumberTest.java
    aggiunta test per
    divide
    inverse
    sqrt

--- IdS_Calculator/src/test/java/Entity/OperationComplexNumberTest.java
+++ IdS_Calculator/src/test/java/Entity/OperationComplexNumberTest.java
@@ -295,3 +295,158 @@ public class OperationComplexNumberTest {
         assertEquals(0.0, result.getReal(), 0.0001);
         assertEquals(0.0, result.getImaginary(), 0.0001);
     }
+ //Test divide 
+    @Test
+    public void testDivideTD01() {
+        System.out.println("testDivideTD01");
+        OperationComplexNumber cn1 = new OperationComplexNumber(1.0, 2.0);
+        OperationComplexNumber cn2 = new OperationComplexNumber(3.0, 4.0);
+        OperationComplexNumber result = cn1.divide(cn2);
+        assertEquals(0.44, result.getReal(), 0.01);
+        assertEquals(0.08, result.getImaginary(), 0.01);
+    }
+
+    @Test
+    public void testDivideTD02() {
+        System.out.println("testDivideTD02");
+        OperationComplexNumber cn1 = new OperationComplexNumber(0.0, 2.0);
+        OperationComplexNumber cn2 = new OperationComplexNumber(0.0, 4.0);
+        OperationComplexNumber result = cn1.divide(cn2);
+        assertEquals(0.500000, result.getReal(), 0.01);
+        assertEquals(0.0, result.getImaginary(), 0.01);
+    }
+
+    @Test
+    public void testDivideTD03() {
+        System.out.println("testDivideTD03");
+        OperationComplexNumber cn1 = new OperationComplexNumber(10.0, -3.0);
+        OperationComplexNumber cn2 = new OperationComplexNumber(0.0, 4.0);
+        OperationComplexNumber result = cn1.divide(cn2);
+        assertEquals(-0.750000, result.getReal(), 0.01);
+        assertEquals(-2.500000, result.getImaginary(), 0.01);
+    }
+
+    @Test
+    public void testDivideTD04() {
+        System.out.println("testDivideTD04");
+        OperationComplexNumber cn1 = new OperationComplexNumber(-7.0, 0.0);
+        OperationComplexNumber cn2 = new OperationComplexNumber(0.0, 4.0);
+        OperationComplexNumber result = cn1.divide(cn2);
+        assertEquals(0.000000, result.getReal(), 0.01);
+        assertEquals(1.750000, result.getImaginary(), 0.01);
+    }
+
+    @Test
+    public void testDivideTD05() {
+        System.out.println("testDivideTD05");
+        OperationComplexNumber cn1 = new OperationComplexNumber(-0.75, -1.5);
+        OperationComplexNumber cn2 = new OperationComplexNumber(0.0, -4);
+        OperationComplexNumber result = cn1.divide(cn2);
+        assertEquals(0.375000, result.getReal(), 0.01);
+        assertEquals(-0.187500, result.getImaginary(), 0.01);
+    }
+// test inverse
+    @Test
+    public void testInverseTI01() {
+        System.out.println("testInverseTI01");
+        OperationComplexNumber cn = new OperationComplexNumber(1.0, 2.0);
+        OperationComplexNumber result = cn.inverse();
+        assertEquals(-1.0, result.getReal(), 0.001);
+        assertEquals(-2.0, result.getImaginary(), 0.001);
+    }
+
+    @Test
+    public void testInverseTI02() {
+        System.out.println("testInverseTI02");
+        OperationComplexNumber cn = new OperationComplexNumber(0.0, 0.0);
+        OperationComplexNumber result = cn.inverse();
+        assertEquals(0.0, result.getReal(), 0.001);
+        assertEquals(0.0, result.getImaginary(), 0.001);
+    }
+// test SQRT
+    @Test
+    public void testSQRTTR01() {
+        System.out.println("sqrt TR01");
+        OperationComplexNumber cn1 = new OperationComplexNumber(1.0, 1.0);
+        OperationComplexNumber result = cn1.sqrt();
+        OperationComplexNumber expResult = new OperationComplexNumber(1.09868, 0.45509);
+        assertEquals(expResult.getReal(), result.getReal(), 0.0001);
+        assertEquals(expResult.getImaginary(), result.getImaginary(), 0.0001);
+
+    }
+
+    @Test
+    public void testSQRTTR02() {
+        System.out.println("sqrt TR02");
+        OperationComplexNumber cn1 = new OperationComplexNumber(1.0, -1.0);
+        OperationComplexNumber result = cn1.sqrt();
+        OperationComplexNumber expResult = new OperationComplexNumber(1.09868, -0.45509);
+        assertEquals(expResult.getReal(), result.getReal(), 0.0001);
+        assertEquals(expResult.getImaginary(), result.getImaginary(), 0.0001);
+
+    }
+
+    @Test
+    public void testSQRTTR03() {
+        System.out.println("sqrt TR03");
+        OperationComplexNumber cn1 = new OperationComplexNumber(-1.0, 1.0);
+        OperationComplexNumber result = cn1.sqrt();
+        OperationComplexNumber expResult = new OperationComplexNumber(0.45509, 1.09868);
+        assertEquals(expResult.getReal(), result.getReal(), 0.0001);
+
+    }
+
+    @Test
+    public void testSQRTTR04() {
+        System.out.println("sqrt TR04");
+        OperationComplexNumber cn1 = new OperationComplexNumber(-1.0, -1.0);
+        OperationComplexNumber result = cn1.sqrt();
+        OperationComplexNumber expResult = new OperationComplexNumber(0.45509, -1.09868);
+        assertEquals(expResult.getReal(), result.getReal(), 0.0001);
+
+    }
+
+    @Test
+    public void testSQRTTR05() {
+        System.out.println("sqrt TR05");
+        OperationComplexNumber cn1 = new OperationComplexNumber(0.0, 2.0);
+        OperationComplexNumber result = cn1.sqrt();
+        OperationComplexNumber expResult = new OperationComplexNumber(1, 1);
+        assertEquals(expResult.getReal(), result.getReal(), 0.0001);
+        assertEquals(expResult.getImaginary(), result.getImaginary(), 0.0001);
+
+    }
+
+    @Test
+    public void testSQRTTR06() {
+        System.out.println("sqrt TR06");
+        OperationComplexNumber cn1 = new OperationComplexNumber(0.0, -2.0);
+        OperationComplexNumber result = cn1.sqrt();
+        OperationComplexNumber expResult = new OperationComplexNumber(1, -1);
+        assertEquals(expResult.getReal(), result.getReal(), 0.0001);
+        assertEquals(expResult.getImaginary(), result.getImaginary(), 0.0001);
+
+    }
+
+    @Test
+    public void testSQRTTR07() {
+        System.out.println("sqrt TR07");
+        OperationComplexNumber cn1 = new OperationComplexNumber(2.0, 0.0);
+        OperationComplexNumber result = cn1.sqrt();
+        OperationComplexNumber expResult = new OperationComplexNumber(1.41421, 0.0);
+        assertEquals(expResult.getReal(), result.getReal(), 0.0001);
+        assertEquals(expResult.getImaginary(), result.getImaginary(), 0.0001);
+
+    }
+
+    @Test
+    public void testSQRTTR08() {
+        System.out.println("sqrt TR08");
+        OperationComplexNumber cn1 = new OperationComplexNumber(-2.0, 0.0);
+        OperationComplexNumber result = cn1.sqrt();
+        OperationComplexNumber expResult = new OperationComplexNumber(0.0, 1.41421);
+        assertEquals(expResult.getReal(), result.getReal(), 0.0001);
+        assertEquals(expResult.getImaginary(), result.getImaginary(), 0.0001);
+
+    }
+}
