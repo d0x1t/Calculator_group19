2521bd6287e77e2edaf69584542dc1ba376fb47c
filsab <96667199+filsab@users.noreply.github.com>
2023-12-12 13:29:30 +0100

    Create OperationCalculator.java

---
+++ IdS_Calculator/src/main/java/calculator/Entity/OperationCalculator.java
@@ -0,0 +1,21 @@
+package Entity;
+
+public interface OperationCalculator {
+
+    public OperationComplexNumber add(OperationComplexNumber other);
+
+    public OperationComplexNumber subtract(OperationComplexNumber other);
+
+    public OperationComplexNumber multiply(OperationComplexNumber other);
+
+    public OperationComplexNumber divide(OperationComplexNumber other);
+
+    public OperationComplexNumber sqrt();
+
+    public OperationComplexNumber inverse();
+
+    public OperationComplexNumber negate();
+
+    @Override
+    public String toString();
+}
