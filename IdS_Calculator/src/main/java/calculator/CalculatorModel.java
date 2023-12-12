491ecbe54238d6e502351a461b6d87b904ede740
fedraggio <153671315+fedraggio@users.noreply.github.com>
2023-12-12 22:36:28 +0100

    handleVariables - CalculatorModel.java

--- IdS_Calculator/src/main/java/calculator/CalculatorModel.java
+++ IdS_Calculator/src/main/java/calculator/CalculatorModel.java
@@ -16,7 +16,50 @@ public final class CalculatorModel {
 
     }
 
-    public void handleVariables(String variable_operator) throws ErrorHandler { }
+    public void handleVariables(String variable_operator) throws ErrorHandler {
+        //prende in input la stringa: operatore e variabile.
+        if (variable_operator == null) {
+            return; //se per qualche motivo gli viene passato null allora ritorna direttamente
+        }
+        if (stack.isEmpty()) {
+            throw new ErrorHandler("The stack is empty", 1); //Siccome tutte le operazioni sulle variabili fanno riferimento allo stack, se  vuoto ritorna errore.
+        }
+
+        String last_complex_number = (String) stack.peek(); //mi prendo l'ultimo elemento inserito nello stack.
+        if (!isValidComplexNumber(last_complex_number)) { //controllo se non è un operatore
+            throw new ErrorHandler("Last element in the stack is not a complex number", 3);
+        }
+        //se sono qui sono sicuro che ho un tipo "<x".
+        char Variable = Character.toLowerCase(variable_operator.charAt(1));
+
+        switch (variable_operator.charAt(0)) {
+            case '>':
+                bufVariable.storeVariable(Variable, last_complex_number);
+                break;
+            case '<':
+                if (!bufVariable.getVariablesMap().containsKey(Variable)) {
+                    throw new ErrorHandler("The variable is not been initialized", 1);
+                }
+                stack.push(bufVariable.valueVariable(Variable));
+                break;
+            case '+':
+                if (!bufVariable.getVariablesMap().containsKey(Variable)) {
+                    throw new ErrorHandler("The variable is not been initialized", 1);
+                }
+                OperationComplexNumber a1 = new OperationComplexNumber(bufVariable.valueVariable(Variable));
+                OperationComplexNumber b1 = new OperationComplexNumber(last_complex_number);
+                bufVariable.storeVariable(Variable, a1.add(b1).toString());
+                break;
+            case '-':
+                if (!bufVariable.getVariablesMap().containsKey(Variable)) {
+                    throw new ErrorHandler("The variable is not been initialized", 1);
+                }
+                OperationComplexNumber a2 = new OperationComplexNumber(bufVariable.valueVariable(Variable));
+                OperationComplexNumber b2 = new OperationComplexNumber(last_complex_number);
+                bufVariable.storeVariable(Variable, a2.subtract(b2).toString());
+                break;
+        }
+    }
 
     public void executeOperations() throws ErrorHandler {}
 
