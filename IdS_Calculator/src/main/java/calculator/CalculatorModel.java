1c98406260ecb7531a030019542e26015afa8d8f
fedraggio <153671315+fedraggio@users.noreply.github.com>
2023-12-12 22:43:42 +0100

    executeOperations e showAChoiceDialogue - CalculatorModel.java

--- IdS_Calculator/src/main/java/calculator/CalculatorModel.java
+++ IdS_Calculator/src/main/java/calculator/CalculatorModel.java
@@ -61,9 +61,156 @@ public final class CalculatorModel {
         }
     }
 
-    public void executeOperations() throws ErrorHandler {}
+    public void executeOperations() throws ErrorHandler {
+        Stack temp = new Stack(controller.StackTextArea);
+        boolean foundOperator;
+        while (true) {
+            foundOperator = false;
 
-    public int showAChoiceDialogue() {}
+            for (String item : stack.getStack()) {  //APPENA TROVA UN OPERATORE ESCE DAL FOR
+                if (isValidOperator(item)) {
+                    foundOperator = true;
+                    break;
+                }
+            }
+
+            if (!foundOperator && !temp.isEmpty()) { //ENTRA SOLO SE NON CI SONO PIU OPERATORI NELLO STACK E SE LO STACK TEMP è VUOTO              
+                while (!temp.isEmpty()) {            //IN QEUSTO MODO AFFINCHE UN OPERATORE DALLO STACK TEMP NON VIENE SPOSTATO NELLO STACK ORIGINALE
+                    String item = temp.pop();        //SI PUO CONTINUARE AD ITERARE SU TEMP SPOSTANDO NUMERI COMPLESSI NELLO STACK ORIGINALE
+                    if (item.equals("+")
+                            || item.equals("-")
+                            || item.equals("×")
+                            || item.equals("÷")
+                            || item.equals("√")
+                            || item.equals("+/-")) {
+                        stack.push(item);
+                        foundOperator = true;
+                        break;
+                    } else {
+                        stack.push(item);
+                    }
+                }
+            }
+
+            if (!foundOperator && temp.isEmpty()) {  //PUNTO DI USCITA DALLA FUNZIONE
+                break;                               //SE NON CI SONO OPERATORI NELLO STACK E LO STACK TEMP è VUOTO SI PUO TERMINARE.
+            }
+
+            String item = stack.pop();              //LEGGO ED ELIMINO IL TOP DALLO STACK
+            if (item.equals("+")
+                    || item.equals("-")
+                    || item.equals("×")
+                    || item.equals("÷")) {
+
+                if (stack.size() < 2) {
+                    // NON CI SONO ABBASTANZA OPERANDI, RIMETTI L'OPERATORE NELLO STACK E TERMINA LANCIANO L'ECCEZIONE
+                    stack.push(item);
+                    throw new ErrorHandler("Insufficient operands in the stack", 1);
+                }
+
+                String operator = item;
+                String _1 = stack.pop();
+                String _2 = stack.pop();
+
+                // SE LE PROSSIME DUE STRINGHE NON SONO NUMERI COMPLESSI RIMETTILE NELLO STACK, MENTRE L'OPERATORE, CHE è IL TOP, NELLO STACK TEMP 
+                //IN QUESTO MODO ALLA PROSSIMA ITERAZIONE IL TOP DELLO STACK SARà L'ELEMENTO SUCCESSIVO
+                if (!isValidComplexNumber(_1) || !isValidComplexNumber(_2)) {
+                    stack.push(_2);
+                    stack.push(_1);
+                    temp.push(item);
+                    continue;
+                }
+                //******************************************************//
+                //GESTIONE OPERAZIONI BINARIE//
+                OperationComplexNumber a = new OperationComplexNumber(_1);
+                OperationComplexNumber b = new OperationComplexNumber(_2);
+                switch (operator) {
+                    case "+":
+                        stack.push(b.add(a).toString());
+                        break;
+                    case "-":
+                        stack.push(b.subtract(a).toString());
+                        break;
+                    case "×":
+                        stack.push(b.multiply(a).toString());
+                        break;
+                    case "÷":
+                        if (a.getReal() == 0 && a.getImaginary() == 0) {
+                            throw new ErrorHandler("Division impossible", 1);
+
+                        }
+                        stack.push(b.divide(a).toString());
+                        break;
+                }
+                //******************************************************//
+            } else if (item.equals("√")
+                    || item.equals("+/-")) {
+                String operator = item;
+
+                if (stack.size() < 1) {
+                    // NON CI SONO ABBASTANZA OPERANDI, RIMETTE L'OPERATORE NELLO STACK E TERMINA LANCIANDO L'ECCEZIONE      
+                    stack.push(item);
+                    throw new ErrorHandler("Insufficient operands in the stack", 1);
+                }
+
+                String _1 = stack.pop();
+
+                // SE LA PROSSIMA STRINGA NON è UN NUMERO COMPLESSO LO RIMETTE NELLO STACK, MENTRE L'OPERATORE, CHE è IL TOP, NELLO STACK TEMP 
+                //IN QUESTO MODO ALLA PROSSIMA ITERAZIONE IL TOP DELLO STACK SARà L'ELEMENTO SUCCESSIVO
+                if (!isValidComplexNumber(_1)) {
+                    stack.push(_1);
+                    temp.push(item);
+                    continue;
+                }
+                //******************************************************//
+                //GESTIONE OPERAZIONI UNARIE//
+                OperationComplexNumber a = new OperationComplexNumber(_1);
+
+                switch (operator) {
+                    case "√":
+                        stack.push(a.sqrt().toString());
+                        if (a.sqrt().getImaginary() != 0.0) {
+                            stack.push(a.sqrt().inverse().toString());
+                        }
+                        break;
+                    case "+/-":
+                        stack.push(a.inverse().toString());
+                        break;
+                }
+                //******************************************************//
+            } else {
+                temp.push(item); //SE SI è ARRIVATI QUI ALLORA IL TOP DELLO STACK è UN NUMERO COMPLESSO E NON UN OPERATORE
+            }
+
+        }
+    }
+
+    public int showAChoiceDialogue() {
+        System.out.println(stack.size());
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Scelta Operazione");
+        alert.setHeaderText("Considerare 'j' come:");
+        alert.setContentText("Scegli l'opzione corretta:");
+
+        ButtonType btnVariabile = new ButtonType("Variabile");
+        ButtonType btnNumeroComplesso = new ButtonType("Numero Complesso");
+
+        // Imposta i bottoni sul dialogo
+        alert.getButtonTypes().setAll(btnVariabile, btnNumeroComplesso);
+
+        // Mostra il dialogo e attendi la risposta dell'utente
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent()) {
+            if (result.get() == btnVariabile) {
+                // L'utente ha scelto "Variabile"
+                return 1;
+            } else if (result.get() == btnNumeroComplesso) {
+                // L'utente ha scelto "Numero Complesso"
+                return 2;
+            }
+        }
+        return 0;
+    }
 
     public boolean isValidComplexNumber(String text) {}
 
