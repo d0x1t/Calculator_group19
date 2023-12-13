824c438e1255c7a6c8938750b88d49a2b3af92c2
fedraggio <153671315+fedraggio@users.noreply.github.com>
2023-12-13 19:02:29 +0100

    Update ErrorHandler.java

--- IdS_Calculator/src/main/java/calculator/ErrorHandler.java
+++ IdS_Calculator/src/main/java/calculator/ErrorHandler.java
@@ -5,6 +5,33 @@ import javafx.scene.control.TextField;
 
 
 public class ErrorHandler extends Exception {
+    public class ErrorHandler extends Exception {
+
+    private void resetTextFieldAfterDelay(TextField textField, String initialText, long delay, TimeUnit unit) {
+        if (textField == null || initialText == null || delay <= 0 || CalculatorView.controller == null) {
+            return;
+        }
+
+        CalculatorView.scheduler.schedule(() -> {
+            Platform.runLater(() -> {
+                CalculatorView.controller.enableDisableAllKeys(false);
+                textField.setText(initialText);
+            });
+        }, delay, unit);
+    }
+
+    private void displayMessage(String message, long seconds) {
+        if (message == null || seconds <= 0 || CalculatorView.controller == null) {
+            return;
+        }
+
+        CalculatorView.controller.enableDisableAllKeys(true);
+        String bak_string = CalculatorView.controller.getMainTextFieldText();
+        CalculatorView.controller.setMainTextFieldText(message);
+        resetTextFieldAfterDelay(CalculatorView.controller.getMainTextField(), bak_string, seconds, TimeUnit.SECONDS);
+
+    }
+
     public ErrorHandler() {
         displayMessage("Generic error", 1);
     }
@@ -19,12 +46,4 @@ public class ErrorHandler extends Exception {
         displayMessage(errorMessage, 1);
     }
 
-
-   private void resetTextFieldAfterDelay(TextField textField, String initialText, long delay, TimeUnit unit) {
-    }
-
-    private void displayMessage(String message, long seconds) {
-
-
-    }
 }
