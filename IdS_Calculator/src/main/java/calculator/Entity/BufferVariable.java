b69e23aa06b98a9d97050c3ce2fe42eb675804ce
dano20zombie <36007523+dano20zombie@users.noreply.github.com>
2023-12-21 14:58:56 +0100

    Update BufferVariable.java
    End BufferVariable class

--- IdS_Calculator/src/main/java/calculator/Entity/BufferVariable.java
+++ IdS_Calculator/src/main/java/calculator/Entity/BufferVariable.java
@@ -1,29 +1,53 @@
-package calculator.Entity;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
 import java.util.HashMap;
 import java.util.Map;
 import javafx.application.Platform;
 import javafx.scene.control.TextArea;
 
-
+/**
+ *
+ * @author ACER
+ */
 public class BufferVariable {
-
-    private Map<Character, String> variables;
+    private  Map<Character, String> variables;
     private TextArea textArea;
-
     public BufferVariable(TextArea textArea) {
         variables = new HashMap<>();
-        this.textArea = textArea;
-
+        this.textArea=textArea;
+        
     }
-    public Map<Character, String> getVariablesMap() {
+    
+    public Map<Character, String> GetVariablesMap(){
         return variables;
     }
-
-    public String valueVariable(char variableName) {
-    return null;
+        public String valueVariable(char variableName) {
+        return variables.get(variableName); 
+        
     }
+    public void storeVariable(char variableName, String value) {
+            variables.put(variableName, value); 
+            updateTextArea();
+            
+    }
+    public void updateTextArea(){
+        StringBuilder sb = new StringBuilder();
+        // Itera le variabili dalla prima all'ultima
+        for (int i = 'a'; i <= 'z'; i++) {
+            String complex_number = this.variables.get((char)i);
+            if (complex_number != null)
+                sb.append((char)i).append(": ").append(complex_number).append("\n");
+        }
+        String text = sb.toString();
+        
+        if (text == null || textArea == null) return;
 
-    public void storeVariable(char variableName, String value) {}
-
-    public void updateTextArea() {}
+        // Aggiorna la TextArea sul thread dell'interfaccia utente
+        Platform.runLater(() -> textArea.setText(text));
+    }
 }
